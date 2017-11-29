77ec918a5e0ea28afa329645c6e916c1ed7d0b99
Aaron Karlsruher <aaronkarlsruher@gmail.com>
2017-11-29 14:17:33 -0500

    start vid 3 sect 2

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyCzhtakiKoBaZIZkKJ7u737_64jm0sLa_k';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-	console.log(data);
-});
-
 //Create a new component.  This component should produce some HTML.
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			//console.log(data);
+			this.setState({ videos });
+		});
+	}
+	render(){
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
 
